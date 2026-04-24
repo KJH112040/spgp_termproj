ee0df831d7e183734e5b82937232dc915a1c1280
KJH04_ <10wjdgp04@gmail.com>
2026-04-24 19:05:15 +0900

    start버튼과 게임 activity 생성

--- projFile/app/src/main/java/kr/ac/tukorea/spgp2026/MainActivity.kt
+++ projFile/app/src/main/java/kr/ac/tukorea/spgp2026/MainActivity.kt
@@ -1,20 +1,40 @@
 package kr.ac.tukorea.spgp2026
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kr.ac.tukorea.spgp2026.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        if (BuildConfig.DEBUG) {
+            Handler(Looper.getMainLooper()).postDelayed({
+                startGameActivity()
+            }, 1000)
         }
+    }
+
+    fun onStartGameClicked(view: View) {
+        startGameActivity()
+    }
+
+    private fun startGameActivity() {
+        Log.d(javaClass.simpleName, "Start Game")
+        val intent = Intent(this, gameFlyActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ projFile/app/src/main/java/kr/ac/tukorea/spgp2026/gameFlyActivity.kt
@@ -0,0 +1,13 @@
+package kr.ac.tukorea.spgp2026
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class gameFlyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
