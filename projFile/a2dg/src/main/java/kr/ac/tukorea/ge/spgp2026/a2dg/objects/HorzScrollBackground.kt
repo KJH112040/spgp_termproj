3e0da07e3366b80dec1654f0603454a78fe3834b
KJH04_ <10wjdgp04@gmail.com>
2026-04-24 18:07:42 +0900

    프로젝트 생성

---
+++ projFile/a2dg/src/main/java/kr/ac/tukorea/ge/spgp2026/a2dg/objects/HorzScrollBackground.kt
@@ -0,0 +1,17 @@
+package kr.ac.tukorea.ge.spgp2026.a2dg.objects
+
+import android.graphics.Canvas
+import kr.ac.tukorea.ge.spgp2026.a2dg.view.GameContext
+
+open class HorzScrollBackground (
+    gctx: GameContext,
+    resId: Int,
+    private val speed: Float,
+) : Sprite(gctx, resId) {
+    private val screenWidth = gctx.metrics.width
+    private val screenHeight = gctx.metrics.height
+
+    init{
+        
+    }
+}
