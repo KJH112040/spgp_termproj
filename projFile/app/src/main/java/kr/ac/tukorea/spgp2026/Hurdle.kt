cef6d94d55590157d76d597018bbaba28938882f
KJH04_ <10wjdgp04@gmail.com>
2026-04-30 17:59:47 +0900

    장애물 움직임

--- projFile/app/src/main/java/kr/ac/tukorea/spgp2026/Hurdle.kt
+++ projFile/app/src/main/java/kr/ac/tukorea/spgp2026/Hurdle.kt
@@ -15,9 +15,9 @@ class Hurdle /*private constructor*/(
     private val TOP_PIPE = Rect(0,0,26,161)
     private val BOTTOM_PIPE = Rect(28,0,54,161)
     override var width = HURDLE_WIDTH
-    override var height = 100f
-    override var x = gctx.metrics.width/2//-HURDLE_WIDTH / 2f
-    override var y = 0f
+    var gap = 150f
+    override var x = gctx.metrics.width + HURDLE_WIDTH / 2f
+    override var y = 1000f
     private var speed = DEFAULT_SPEED
     private val dstTop = RectF()
     private val dstBottom = RectF()
@@ -33,21 +33,30 @@ class Hurdle /*private constructor*/(
 
 
     override fun update(gctx: GameContext) {
+        x -= speed * gctx.frameTime
+
         dstTop.set(
             x - width / 2f,
-            y - height / 2f,
+            0f,
             x + width / 2f,
-            y + height / 2f,
+            y - gap / 2f,
+        )
+        dstBottom.set(
+            x - width / 2f,
+            y + gap / 2f,
+            x + width / 2f,
+            gctx.metrics.height
         )
     }
     override fun draw(canvas: Canvas) {
-        canvas.drawBitmap(bitmap,TOP_PIPE,dstTop,null)
+        canvas.drawBitmap(bitmap,TOP_PIPE,dstTop, null)
+        canvas.drawBitmap(bitmap,BOTTOM_PIPE,dstBottom,null)
 
     }
     override fun onRecycle() {
     }
     companion object{
-        const val HURDLE_WIDTH = 100f
+        const val HURDLE_WIDTH = 150f
         const val DEFAULT_SPEED = 50f
 //        fun get(gctx: GameContext): Hurdle{
 //            val hurdle = Hurdle()
