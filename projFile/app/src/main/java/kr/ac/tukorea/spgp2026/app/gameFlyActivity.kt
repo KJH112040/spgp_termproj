d80c404e5e7551bfa5239ad49177ee0078a665f2
KJH04_ <10wjdgp04@gmail.com>
2026-06-12 23:09:38 +0900

    디버그 비활성화

--- projFile/app/src/main/java/kr/ac/tukorea/spgp2026/app/gameFlyActivity.kt
+++ projFile/app/src/main/java/kr/ac/tukorea/spgp2026/app/gameFlyActivity.kt
@@ -6,9 +6,9 @@ import kr.ac.tukorea.ge.spgp2026.a2dg.view.GameContext
 import kr.ac.tukorea.spgp2026.Game.scenes.MainScene
 
 class gameFlyActivity : BaseGameActivity() {
-    override val drawsDebugGrid = true
-    override val drawsDebugInfo = true
-    override val drawsFpsGraph = true
+//    override val drawsDebugGrid = true
+//    override val drawsDebugInfo = true
+//    override val drawsFpsGraph = true
 
     override fun createRootScene(gctx: GameContext): Scene {
         val birdID = intent.getIntExtra(KEY_BIRD_ID, 0)
